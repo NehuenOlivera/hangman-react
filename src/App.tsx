c85071c4e288fa362f0283d8baa7bda668dc7bea
Nehuen Olivera <nehuenolivera@gmail.com>
2025-11-04 16:55:52 +1000

    set states needed for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+import words from './wordList.json'
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    const randomIndex = Math.floor(Math.random() * words.length)
+    return words[randomIndex]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   return <h1>Hi</h1>
 }
 
