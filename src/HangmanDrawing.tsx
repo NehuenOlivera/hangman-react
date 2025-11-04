e1aadce52b938900e3599a51cb4d8ca9bfb957fa
Nehuen Olivera <nehuenolivera@gmail.com>
2025-11-04 17:37:53 +1000

    Adding Hangman drawing

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,94 @@
+const HEAD = (
+    <div style={{
+        width: '50px',
+        height: '50px',
+        borderRadius: "100%",
+        border: '10px solid black',
+        position: 'absolute',
+        top: '50px',
+        right: '-30px',
+    }}/>
+)
+
+const BODY = (
+    <div style={{
+        width: '10px',
+        height: '100px',
+        background: 'black',
+        position: 'absolute',
+        top: '118px',
+        right: 0,
+    }}/>
+)
+
+const RIGHT_ARM = (
+    <div style={{
+        width: '100px',
+        height: '10px',
+        background: 'black',
+        position: 'absolute',
+        top: '150px',
+        right: '-100px',
+        rotate: '-30deg',
+        transformOrigin: 'left bottom',
+    }}/>
+)
+
+const LEFT_ARM = (
+    <div style={{
+        width: '100px',
+        height: '10px',
+        background: 'black',
+        position: 'absolute',
+        top: '150px',
+        right: '10px',
+        rotate: '30deg',
+        transformOrigin: 'right bottom',
+    }}/>
+)
+
+const RIGHT_LEG = (
+    <div style={{
+        width: '100px',
+        height: '10px',
+        background: 'black',
+        position: 'absolute',
+        top: '210px',
+        right: '-90px',
+        rotate: '40deg',
+        transformOrigin: 'left bottom',
+    }}/>
+)
+
+const LEFT_LEG = (
+    <div style={{
+        width: '100px',
+        height: '10px',
+        background: 'black',
+        position: 'absolute',
+        top: '210px',
+        right: 0,
+        rotate: '-40deg',
+        transformOrigin: 'right bottom',
+    }}/>
+) 
+
+
+export function HangmanDrawing() {
+    return <div style={{ position: 'relative'}}>
+        {HEAD}
+        {BODY}
+        {RIGHT_ARM}
+        {LEFT_ARM}
+        {RIGHT_LEG}
+        {LEFT_LEG}
+        {/* Hanging part */}
+        <div style={{ height: '50px', width: '10px', background: 'black', position:'absolute', right: 0}}></div>
+        {/* Top */}
+        <div style={{ height: '10px', width: '200px', background: 'black', marginLeft: '120px'}}></div>
+        {/* Pole */}
+        <div style={{ height: '400px', width: '10px', background: 'black', marginLeft: '120px'}}></div>
+        {/* Base */}
+        <div style={{ height: '10px', width: '250px', background: 'black'}}></div>
+    </div>
+}
