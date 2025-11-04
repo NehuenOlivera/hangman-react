1d29e561650b73017e85bb139cf267c1f272380a
Nehuen Olivera <nehuenolivera@gmail.com>
2025-11-04 18:20:32 +1000

    Display Keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,34 @@
-import { useState } from 'react'
-import words from './wordList.json'
-import { HangmanDrawing } from './HangmanDrawing'
-import { HangmanWord } from './HangmanWord'
-import { Keyboard } from './Keyboard'
+import { useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    const randomIndex = Math.floor(Math.random() * words.length)
-    return words[randomIndex]
-  })
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
     <div
       style={{
-        maxWidth: '800px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-        margin: '0 auto',
-        alignItems: 'center'
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
       }}
     >
-      <div style={{ fontSize: '2rem', textAlign: "center"}}>
-        Lose
-        Win
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
       </div>
-      <HangmanDrawing/>
-      <HangmanWord/>
-      <Keyboard/>
-
     </div>
-  )
+  );
 }
-export default App
+export default App;
