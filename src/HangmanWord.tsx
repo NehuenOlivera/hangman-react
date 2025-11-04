469db026bb693aa559338a1bed76e71282e57726
Nehuen Olivera <nehuenolivera@gmail.com>
2025-11-04 17:56:44 +1000

    Show Word to guess

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,31 @@
 export function HangmanWord() {
-    
+  const word = "test";
+  const guessedLetters: string[] = ["t"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {/* Split the word into letters and display each letter */}
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
